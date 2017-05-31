d94e90c1f239396d92c7c71085d177d9f510e3e8
Tony Santos <tony@simppler.com>
2017-05-31 10:00:35 -0700

    made some progress on the configuartion of the assemble file w/o much documentation to fall back on. Gulp plugins were no problem, still stuck on getting partials registered

--- assemblefile.js
+++ assemblefile.js
@@ -1,14 +1,16 @@
 'use strict';
 
 var assemble = require('assemble');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 var app = assemble();
-
-// app.layouts('/layouts/*.hbs');
 
 app.task('init',function( cb ){
   app.pages('templates/*.hbs');
   app.create('layouts',{viewType: 'layout'});
+  app.create('partials',{viewType: 'partial'});
   app.layouts('./layouts/*.hbs');
+  app.partials('./partials/**/*.hbs');
 
   cb();
 });
@@ -18,6 +20,18 @@ app.task('assemble',function(){
     .pipe(app.renderFile())
     .pipe(app.dest('dist'));
 });
-app.task('default', ['init','assemble']);
+
+// The sass task autoprefixes the resulting CSS before writing it.
+app.task('sass', function(){
+  return app.src('./assets/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      grid: true
+    }))
+    .pipe(app.dest('./dist/css'));
+});
+
+app.task('default', ['init','sass','assemble']);
 
 module.exports = app;
