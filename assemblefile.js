accfd3d90247afa74ed4c513d5079fe59dec72dc
Tony Santos <tony@notjustprettypictures.com>
2017-06-24 15:50:19 -0700

    Added watch and browsersync, clarified source directory names
    Made the changes to include watch and browsersync support so now
    everything on the assemblefile should be done, I think. Renamed the
    templates folder to pages because it confused even me which was which
    (templates v layouts) after a couple weeks off.

--- assemblefile.js
+++ assemblefile.js
@@ -6,14 +6,19 @@ var autoprefixer = require('gulp-autoprefixer');
 var extname = require('gulp-extname')
 var helpers = require('handlebars-helpers')();
 var yml = require('js-yaml');
+var browserSync = require('browser-sync').create();
+var watch = require('base-watch');
+var path = require('path');
+
 var app = assemble();
+app.use(watch());
 
 
 
 app.task('init',function( cb ){
   // pages, layouts, and partials ship with assemble so I only need to point them at the right files
   // using .create() is what was making it not work :s
-  app.pages('*.hbs',{cwd: 'templates'});
+  app.pages('*.hbs',{cwd: 'pages'});
   app.layouts('*.hbs', {cwd: 'layouts'});
   app.partials('**/*.hbs', {cwd: 'partials'});
   // load all the yml or json files in the data folder as data, namespaced w/ their filename
@@ -21,16 +26,29 @@ app.task('init',function( cb ){
     return yml.safeLoad(str);
   });
   app.data(['**/*.yml','**/*.json'], {cwd:'data', namespace: true});
-  console.log(app.cache.data);
+  // console.log(app.cache.data);
   app.helpers(helpers);
   cb();
 });
 
-app.task('assemble',function(){
+// the task to actually build the handlebars files into html
+app.task('assemble', ['init'], function(){
   return app.toStream('pages')
     .pipe(app.renderFile())
+    .on('err', console.error)
     .pipe(extname())
-    .pipe(app.dest('dist/'));
+    .pipe(app.dest(path.join(__dirname,'./dist')))
+    .pipe(browserSync.stream());
+});
+
+app.task('serve',function(){
+  browserSync.init({
+    port:8080,
+    startPath: 'index.html',
+    server: {
+      baseDir: path.join(__dirname, './dist')
+    }
+  });
 });
 
 // The sass task autoprefixes the resulting CSS before writing it.
@@ -41,9 +59,13 @@ app.task('sass', function(){
       browsers: ['last 2 versions'],
       grid: true
     }))
-    .pipe(app.dest('./dist/css'));
+    .pipe(app.dest(path.join(__dirname,'./dist/css')))
+    .pipe(browserSync.stream());
 });
 
-app.task('default', ['init','sass','assemble']);
+app.task('default', ['sass','assemble','serve']);
+
+app.watch(path.join(__dirname,'./pages/**/*.{hbs}'),['assemble']);
+app.watch(path.join(__dirname,'./assets/sass/**/*.{scss}'),['sass']);
 
 module.exports = app;
