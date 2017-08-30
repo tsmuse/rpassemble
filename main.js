954557ad1b46aaf77d8fc74c9168662fb3e39aad
Tony Santos <tony@notjustprettypictures.com>
2017-08-30 13:46:55 -0700

    More tweaks in-between other stuff
    At this rate I’ll have a ready to templateize front-end in 15 or 20
    more meetings lol

--- main.js
+++ main.js
@@ -90,7 +90,7 @@ ipc.on('open-file-dialog-sheet', function(event){
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 780, height: 600})
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
